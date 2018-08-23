6bbcbcc0f3f57d98b766657460bf56dc6b200fd6
Olivier Willems <ojwillems@gmail.com>
2018-08-23 23:45:07 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,8 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthdays_kids)
-  birthdays_kids.each do |kids_name, age|
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
